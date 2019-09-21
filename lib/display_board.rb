ef114da59cc39aa79be86c4b14c120183bb2f0b7
Eli Kantor <eli.kantor@outlook.com>
2019-09-21 18:53:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board
+  
+  puts "{board[0]"|
+
 # Define display_board that accepts a board and prints
 # out the current state.
